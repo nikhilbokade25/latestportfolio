04cbeb68e66a414c566883aad9b8ac9c56057203
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-03-13 11:06:39 -0400

    Implement animation to Hero Component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,25 +1,39 @@
+import React, { useEffect, useRef } from 'react';
 import './Hero.css';
 import portfolioImg from "../Assets/portfolioImg2.jpg";
+import gsap from 'gsap';
 
-export default function Hero(){
-    return(
+export default function Hero() {
+    const revealDivRef = useRef(null);
+
+    useEffect(() => {
+        gsap.fromTo(revealDivRef.current, {
+            height: '100vh',
+            
+        }, {
+            height: '0vh',
+            duration: 1.5,
+            ease: 'power4.in',
+        });
+    }, []);
+
+    return (
         <div className="hero_container">
             <div className="hero_content">
                 <div className="hero_text">
                     <h1>Hello! I'm Nikhil.</h1>
                     <p>A front-end developer who’s passionate about crafting fun and engaging user experiences. He loves going to concerts, gazing at the sky and appreciating things made to last.</p>
-
                 </div>
 
                 <div className="hero_image">
                     <div className="portfolio_image">
-                        <img src={portfolioImg} />
+                        <img src={portfolioImg} alt="Portfolio" />
                     </div>
                 </div>
             </div>
-
-            
-            
+            <div className="reveal_div" ref={revealDivRef}>
+                
+            </div>
         </div>
     );
 }
