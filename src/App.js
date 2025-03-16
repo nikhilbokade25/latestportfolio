7156ffa0172c9c7483f08340c71b22b16d0fe354
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-03-16 17:24:54 -0400

    Add Collab Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Collab from './Components/Collab';
 import Hero from './Components/Hero';
 import Projects from './Components/Projects';
+
 
 function App() {
   return (
     <div className="App">
         <Hero/>
         <Projects />
+        <Collab/>
     </div>
   );
 }
---
+++ src/Components/Collab.jsx
@@ -0,0 +1,16 @@
+import './Collab.css';
+
+export default function Collab(){
+    return(
+        <div className="collab_container">
+            <div className='collab_heading'>
+                <h1>Ready to launch your vision?</h1>
+            </div>
+            <div className='collab_text'>
+                <p>Nikhil is excited to help bring your web development vision to life! I'm ready to collaborate and contribute my front-end skills to your next impactful project.</p>
+                <button>Collab with Nikhil</button>
+            </div>
+        </div>
+
+    );
+}
