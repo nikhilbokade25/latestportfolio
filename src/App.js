15adef50ab90482cb98dd083fd4d9ac2a477399a
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-03-16 20:07:28 -0400

    Changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+// import './App.css';
+// import Collab from './Components/Collab';
+// import Footer from './Components/Footer';
+// import Hero from './Components/Hero';
+// import Projects from './Components/Projects';
+// import About from './Components/About';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// function App() {
+//     return (
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={
+//                     <>
+//                         <Hero />
+//                         <Projects />
+//                         <Collab />
+//                         <Footer />
+//                     </>
+//                 } />
+//                 <Route path="/about" element={<About />} />
+//             </Routes>
+//         </Router>
+//     );
+// }
+
+// export default App;
+
+
 import './App.css';
 import Collab from './Components/Collab';
 import Footer from './Components/Footer';
@@ -8,7 +37,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
-        <Router>
+        <Router basename="/latestportfolio"> 
             <Routes>
                 <Route path="/" element={
                     <>
